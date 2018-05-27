28660f916f77877695bf6e131d5deb8d0ac70cd4
weili <lixiaoming789@126.com>
2018-05-27 23:15:38 +0800

    Add project files.

---
+++ hellomake.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hellomake.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年05月27日 星期日 22时09分12秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include "hellomake.h"
+
+void print_hello(void)
+{
+    printf("hello makefiles!\n");
+    return;
+}
---
+++ hellomake.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hellomake.h
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年05月27日 星期日 22时08分13秒
+ ************************************************************************/
+
+#ifndef _HELLOMAKE_H
+#define _HELLOMAKE_H
+
+void print_hello(void);
+
+#endif
